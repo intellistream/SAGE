a09ea35ed08de6e678a0e8ec812dc0231608c9a5
Mao Yancan <maoyancan@u.nus.edu>
2025-03-29 23:32:58 +0800

    Upper layer APP + API unit test completed, TODO: query_api.py imports from relative path, may cause depdnency issues in production

--- sage/api/operator/writer_function_api.py
+++ sage/api/operator/writer_function_api.py
@@ -1,7 +1,9 @@
-class WriterFunction:
+from sage.api.operator.base_operator_api import BaseOperator
+
+
+class WriterFunction(BaseOperator):
     def __init__(self):
-        self.upstream = None
-        self.downstream = None
+        super().__init__()
 
     def execute(self, inputs, context=None):
         raise NotImplementedError("WriterFunction must implement execute().")
