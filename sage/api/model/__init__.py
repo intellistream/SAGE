70c331c2096ba2e57d79f001485ff76575e9783a
iliujunn <iliujun@msn.com>
2025-04-01 08:25:43 +0000

    resolve conflicts

--- sage/api/model/__init__.py
+++ sage/api/model/__init__.py
@@ -1,5 +1,2 @@
 from .model_api import apply_generator_model, apply_embedding_model
 __all__ = ["apply_generator_model", "apply_embedding_model"]
-
-# from .model_api import apply_embedding_model
-# __all__ = [ "apply_embedding_model"]
