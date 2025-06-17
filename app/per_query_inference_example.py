2e24a922b5ba258585c904448b110a211528ce89
lpl <2086388579@qq.com>
2025-06-17 17:57:57 +0800

    pipeline_exam

--- app/per_query_inference_example.py
+++ app/per_query_inference_example.py
@@ -1,36 +1,52 @@
 import time
-
 import sage
 import logging
-# Assume the query module exposes execute() to handle a single query
-import ray
 import yaml
+from sage.core.neuromem.memory_manager import MemoryManager
+from sage.core.neuromem.test.embeddingmodel import MockTextEmbedder
 
 
-ray.init(
-    logging_level=logging.CRITICAL,
-)
 logging.basicConfig(level=logging.DEBUG)
 def load_config(path: str) -> dict:
     with open(path, 'r') as f:
         return yaml.safe_load(f)
 
-config = load_config('./app/config.yaml')
-logging.basicConfig(level=logging.ERROR)
 
-manager = sage.memory.init_default_manager()
-config["memory_manager"]=manager
-memory = sage.memory.create_table("long_term_memory", manager=manager)
+def memory_init():
+    """初始化内存管理器并创建测试集合"""
+    default_model = MockTextEmbedder(fixed_dim=128)
+    manager = MemoryManager()
+    col = manager.create_collection(
+        name="vdb_test",
+        backend_type="VDB",
+        embedding_model=default_model,
+        dim=128,
+        description="test vdb collection",
+        as_ray_actor=False,
+    )
+    col.add_metadata_field("owner")
+    col.add_metadata_field("show_type")
+    texts = [
+        ("hello world", {"owner": "ruicheng", "show_type": "text"}),
+        ("你好，世界", {"owner": "Jun", "show_type": "text"}),
+        ("こんにちは、世界", {"owner": "Lei", "show_type": "img"}),
+    ]
+    for text, metadata in texts:
+        col.insert(text, metadata)
+    col.create_index(index_name="vdb_index")
+    config_for_qa["retriever"]["ltm_collection"] = col
 
-while(True):
-    user_input = input("\n>>> ").strip()
-    if user_input.lower() == "exit":
-        logging.info("Exiting SAGE Interactive Console")
-        print("Goodbye!")
-        break
-    time1 = time.time()
-    sage.query.run_query(user_input,config)
-    time2 = time.time()
-    logging.info(f"Query executed in {time2 - time1:.4f} seconds")
+if __name__ == '__main__':
+
+    config_for_qa= load_config('./app/config_for_qa.yaml')
+    memory_init()
+    while(True):
+        user_input = input("\n>>> ").strip()
+        if user_input.lower() == "exit":
+            logging.info("Exiting SAGE Interactive Console")
+            print("Goodbye!")
+            break
+        sage.query.run_query(user_input,config_for_qa)
+
 
 
