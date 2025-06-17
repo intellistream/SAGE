c270a2dd2d439fd16d8c3dd81a98b62ea1b296fa
CubeLander <1365287231@qq.com>
2025-06-17 08:15:14 +0000

    继续优化项目结构

--- sage/core/runtime/__init__.py
+++ sage/core/runtime/__init__.py
@@ -1,5 +1,7 @@
 from .base_runtime import BaseRuntime
 from .local import LocalRuntime
 from .ray.ray_runtime import RayRuntime
+from .memory_adapter import MemoryAdapter
+
 # 供顶层 sage/__init__.py 使用
-__all__ = ["BaseRuntime", "LocalRuntime", "RayRuntime"]
+__all__ = ["BaseRuntime", "LocalRuntime", "RayRuntime", "MemoryAdapter"]
