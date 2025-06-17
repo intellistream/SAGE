8fa5fcaeb2a11e7b85228c4ac675c13b0ad05c81
CubeLander <1365287231@qq.com>
2025-06-17 07:33:14 +0000

    去除了不必要的抽象raydag_task

--- sage/core/engine/engine.py
+++ sage/core/engine/engine.py
@@ -51,45 +51,6 @@ class Engine:
 
         self.pipeline_to_dag = {}
 
-    # def submit_pipeline(self,pipeline,config=None): # deprecated
-    #     optimized_dag = self.compiler.compile(pipeline,config)
-    #     # execution_type和node_mapping被封装进dag里边当成员变量了
-    #     dag_id=self.dag_manager.add_dag(optimized_dag)
-    #     self.pipeline_to_dag[pipeline]=dag_id
-    #     self.dag_manager.submit_dag(dag_id)
-    #     self.executor_manager.run_dags()
-
-    # def submit_graph(self, graph):
-    #     ray_dag_task = self.compiler.compile_graph(graph)
-    #     from sage.core.engine.ray_execution_backend import RayDAGExecutionBackend
-    #     ray_backend = RayDAGExecutionBackend(monitoring_interval=2.0)
-    #     dag_handle = ray_backend.submit_task(ray_dag_task)
-    #     # Monitor execution status
-    #     import time
-    #     for i in range(10):
-    #         status = ray_backend.get_status(dag_handle)
-    #         print(f"Status: {status}")
-    #         time.sleep(5)
-        
-    #     # For oneshot DAGs, wait for completion
-    #     if ray_dag_task.ray_dag.strategy == "oneshot":
-    #         success = ray_backend.wait_for_completion(dag_handle, timeout=300)
-    #         print(f"Execution completed: {success}")
-    #     else:
-    #         # For streaming DAGs, stop after some time
-    #         time.sleep(60)
-    #         ray_backend.stop_task(dag_handle)
-    #         print("Streaming DAG stopped")
-
-    # def stop_pipeline(self,pipeline):
-    #     dag_id=self.pipeline_to_dag[pipeline]
-    #     self.executor_manager.stop_dag(dag_id)
-
-
-    # def get_executor_manager(self):
-    #     return self.executor_manager
-
-
     def get_dag_manager(self):
         return self.dag_manager
     
