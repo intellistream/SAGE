b21fb6971c32e2de9fd3f17814f57450d899a70e
shuhao <shuhao_zhang@hust.edu.cn>
2025-07-04 08:42:02 +0800

    minor fix

---
+++ sage_frontend/dashboard/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'sage_examples-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {}
+}
