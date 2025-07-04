659b2be34b1830a5edd0ab41a11a922849027db9
CubeLander <1365287231@qq.com>
2025-07-04 09:05:17 +0000

    added an example of calling the env just once

---
+++ sage_examples/qa_oneshot.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import os, time
+from sage_core.api.env import LocalEnvironment
+from sage_common_funs.io.source import FileSource
+from sage_common_funs.io.sink import TerminalSink
+from sage_common_funs.rag.generator import OpenAIGenerator
+from sage_common_funs.rag.promptor import QAPromptor
+from sage_common_funs.rag.retriever import DenseRetriever
+from sage_utils.config_loader import load_config
+from sage_utils.logging_utils import configure_logging
+
+def pipeline_run():
+    """创建并运行数据处理管道"""
+    env = LocalEnvironment()
+    env.set_memory(config = None)
+    # 构建数据处理流程
+    query_stream = (env
+        .from_source(FileSource, config["source"])
+        .map(DenseRetriever, config["retriever"])
+        .map(QAPromptor, config["promptor"])
+        .map(OpenAIGenerator, config["generator"])
+        .sink(TerminalSink, config["sink"])
+    )
+    env.submit()
+    env.run_once()
+    env.run_once()
+    env.run_once()
+
+
+
+    time.sleep(100)  # 等待管道运行
+
+
+if __name__ == '__main__':
+
+
+    # 加载配置
+    config = load_config("config.yaml")
+    load_dotenv(override=False)
+
+    api_key = os.environ.get("ALIBABA_API_KEY")
+    if api_key:
+        config.setdefault("generator", {})["api_key"] = api_key
+
+    pipeline_run()
