c79e804f331784dee21bb937cc5aceb0dcb5f344
CubeLander <1365287231@qq.com>
2025-07-09 08:05:40 +0000

    enhanced dagnode initialization logic

--- sage_runtime/executor/base_dag_node.py
+++ sage_runtime/executor/base_dag_node.py
@@ -3,12 +3,13 @@ import threading
 from typing import Any, TYPE_CHECKING, Union
 from sage_utils.custom_logger import CustomLogger
 from sage_runtime.operator.runtime_context import RuntimeContext
+from ray.actor import ActorHandle
+
 if TYPE_CHECKING:
     from sage_runtime.io.connection import Connection
     from sage_core.core.operator.base_operator import BaseOperator
     from sage_core.api.transformation import Transformation, OperatorFactory
     from sage_core.core.compiler import Compiler, GraphNode
-    from ray.actor import ActorHandle
 
 class BaseDAGNode(ABC):
     def __init__(
