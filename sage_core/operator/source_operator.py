2b3ef2ab542d7d8c016f6c42cf48e3514e3e1970
CubeLander <1365287231@qq.com>
2025-07-16 10:21:53 +0000

    added tests

--- sage_core/operator/source_operator.py
+++ sage_core/operator/source_operator.py
@@ -12,13 +12,10 @@ class SourceOperator(BaseOperator):
         
     def process_packet(self, packet: 'Packet' = None):
         try:
-            if packet is not None or packet.payload is not None:
-                self.logger.warning(f"Operator {self.name} received empty data")
             
-            result = self.function.execute(packet.payload)
+            result = self.function.execute()
             self.logger.debug(f"Operator {self.name} processed data with result: {result}")
-            result_packet = packet.inherit_partition_info(result) if (result is not None) else None
-            if result_packet is not None:
-                self.emit_packet(result_packet)
+            if result is not None:
+                self.emit_packet(Packet(result))
         except Exception as e:
             self.logger.error(f"Error in {self.name}.process(): {e}", exc_info=True)
