e03f9f75e6f20166067f8b362920ab07fb53881c
CubeLander <1365287231@qq.com>
2025-07-16 07:32:23 +0000

    midway

--- sage_runtime/io/packet.py
+++ sage_runtime/io/packet.py
@@ -2,6 +2,9 @@ from typing import TypeVar, Generic, Any
 
 T = TypeVar('T')
 class Packet(Generic[T]):
-    def __init__(self, payload: T, input_index: int = 0):
+    def __init__(self, payload: T, input_index: int = 0, 
+                 partition_key: Any = None, partition_strategy: str = None):
         self.payload:Any = payload
         self.input_index:int = input_index
+        self.partition_key = partition_key          # 新增：分区键
+        self.partition_strategy = partition_strategy # 新增：分区策略
