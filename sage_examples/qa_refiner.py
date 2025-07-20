46bba60464cd03fc50de8d02c26bee3ac69443be
Yanbo Chen <cybber6@gmail.com>
2025-07-20 11:13:39 +0800

    LongRefiner (#249)
    implement LongRefiner

--- sage_examples/qa_refiner.py
+++ sage_examples/qa_refiner.py
@@ -5,7 +5,7 @@ from sage_core.api.env import LocalEnvironment
 from sage_libs.rag.generator import OpenAIGenerator
 from sage_libs.rag.promptor import QAPromptor
 from sage_libs.rag.retriever import DenseRetriever
-from sage_libs.rag.refiner import AbstractiveRecompRefiner
+from sage_plugins.longrefiner_fn.longrefiner_adapter import LongRefinerAdapter
 from sage_common_funs.io.source import FileSource
 from sage_common_funs.io.sink import TerminalSink
 from sage_utils.config_loader import load_config
@@ -23,7 +23,7 @@ def pipeline_run():
     # 构建数据处理流程
     query_stream = (env.from_source(FileSource, config["source"])
                     .map(DenseRetriever, config["retriever"])
-                    .map(AbstractiveRecompRefiner, config["refiner"])  
+                    .map(LongRefinerAdapter,  config["refiner"])
                     .map(QAPromptor, config["promptor"])
                     .map(OpenAIGenerator, config["generator"])
                     .sink(TerminalSink, config["sink"])
@@ -31,8 +31,8 @@ def pipeline_run():
 
     # 提交管道并运行
     env.submit()
-    env.run_streaming()
-    time.sleep(5)
+    env.run_once()
+    time.sleep(50)
     env.close()
 
 
