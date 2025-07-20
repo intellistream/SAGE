37ddc58382f889afa8ba184fcf14fee401d6bd7a
shuhao <shuhao_zhang@hust.edu.cn>
2025-07-20 10:24:54 +0800

    quick update

--- sage_examples/qa_dense_retrieval.py
+++ sage_examples/qa_dense_retrieval.py
@@ -1,5 +1,5 @@
 import time
-from sage_core.api.env import LocalEnvironment
+from sage_core.api.env import LocalEnvironment, RemoteEnvironment
 from sage_common_funs.io.source import FileSource
 from sage_common_funs.io.sink import TerminalSink
 from sage_libs.rag.generator import OpenAIGenerator
@@ -10,7 +10,10 @@ from sage_utils.config_loader import load_config
 
 def pipeline_run():
     """创建并运行数据处理管道"""
-    env = LocalEnvironment()
+    # env = LocalBatchEnvironment() #DEBUG and Batch -- Client 拥有后续程序的全部handler（包括JM）
+    # env = LocalStreamEnvironment() #DEBUG and Streaming
+    env = RemoteBatchEnvironment("JM-IP")  # Deployment to JM. -- Client 不拥有后续程序的全部handler（包括JM）
+    # env = RemoteStreamEnvironment("JM-IP")  # Deployment to JM.
     env.set_memory(config=None)
     # 构建数据处理流程
     query_stream = (env
@@ -18,13 +21,10 @@ def pipeline_run():
                     .map(DenseRetriever, config["retriever"])
                     .map(QAPromptor, config["promptor"])
                     .map(OpenAIGenerator, config["generator"])
-                    .sink(TerminalSink, config["sink"])
+                    .sink(TerminalSink, config["sink"]) # TM (JVM) --> 会打印在某一台机器的console里
                     )
     try:
         env.submit()
-        env.run_once()  # 启动管道
-        time.sleep(15)  # 等待管道运行
-        env.stop()
     finally:
         env.close()
 
