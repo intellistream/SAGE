2cb653e30375aa9146f1757cac7e2ac8a26ae758
shuhao <shuhao_zhang@hust.edu.cn>
2025-07-20 11:02:18 +0800

    quick update

--- sage_examples/qa_dense_retrieval.py
+++ sage_examples/qa_dense_retrieval.py
@@ -11,10 +11,29 @@ from sage_utils.config_loader import load_config
 def pipeline_run():
     """创建并运行数据处理管道"""
     # env = LocalBatchEnvironment() #DEBUG and Batch -- Client 拥有后续程序的全部handler（包括JM）
-    # env = LocalStreamEnvironment() #DEBUG and Streaming
     env = RemoteBatchEnvironment("JM-IP")  # Deployment to JM. -- Client 不拥有后续程序的全部handler（包括JM）
-    # env = RemoteStreamEnvironment("JM-IP")  # Deployment to JM.
+
+    # Batch Environment.
+
+    query_stream = (env
+                    .process(FileSource, config["source"]) # 处理且处理一整个file 一次。
+                    .map(DenseRetriever, config["retriever"])
+                    .map(QAPromptor, config["promptor"])
+                    .map(OpenAIGenerator, config["generator"])
+                    .sink(TerminalSink, config["sink"]) # TM (JVM) --> 会打印在某一台机器的console里
+                    )
+
+
+
+
+
+
+
+    # Streaming Environment.
     env.set_memory(config=None)
+    # env = LocalStreamEnvironment() #DEBUG and Streaming
+    # env = RemoteStreamEnvironment("JM-IP")  # Deployment to JM.
+
     # 构建数据处理流程
     query_stream = (env
                     .from_source(FileSource, config["source"])
