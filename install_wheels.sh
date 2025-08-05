73eac00dcece90834bba617cb7a07c394a7c9cd6
Jingyuan Tian <jingyuantian@hust.edu.cn>
2025-08-05 11:43:47 +0800

    删除了旧的wheel包

--- install_wheels.sh
+++ install_wheels.sh
@@ -3,4 +3,4 @@
 pip install --upgrade pip setuptools wheel
 # Pre-install numpy and scipy to pull binary wheels and avoid source builds
 pip install numpy scipy
-pip install sage --find-links=./build/wheels   --only-binary=:all:
+pip install sage --find-links=./build/wheels   --only-binary=:all: --fast-deps
