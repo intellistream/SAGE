8b658389f28edd15e2621aa5218b543b4134b2f0
Jingyuan Tian <jingyuantian@hust.edu.cn>
2025-08-05 11:13:23 +0800

    added tests

--- build_all_wheels.sh
+++ build_all_wheels.sh
@@ -74,11 +74,17 @@ for root in "${ROOTS[@]}"; do
       echo "Building wheel for $pkg"
       # use no isolation to leverage existing torch install and avoid strict torch==2.3.0
 <<<<<<< HEAD
+<<<<<<< HEAD
       # add --no-build-isolation and set PIP_NO_BUILD_ISOLATION to speed up dependency resolution
       export PIP_NO_BUILD_ISOLATION=1
       export PIP_DISABLE_PIP_VERSION_CHECK=1
 =======
 >>>>>>> 7e283a1 (code cleanups)
+=======
+      # add --no-build-isolation and set PIP_NO_BUILD_ISOLATION to speed up dependency resolution
+      export PIP_NO_BUILD_ISOLATION=1
+      export PIP_DISABLE_PIP_VERSION_CHECK=1
+>>>>>>> 5d3c601 (added tests)
       (cd "$pkg" && python3 -m build --wheel --no-isolation --outdir "$OLDPWD/$OUTPUT_DIR")
     fi
   done
