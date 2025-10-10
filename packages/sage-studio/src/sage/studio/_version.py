644cd40acf53c36ab271d539070c20f8fe0d2689
shuhao zhang <shuhao_zhang@hust.edu.cn>
2025-10-10 21:15:07 +0800

    update version

--- packages/sage-studio/src/sage/studio/_version.py
+++ packages/sage-studio/src/sage/studio/_version.py
@@ -1,7 +1,7 @@
 """Version information for sage-studio package."""
 
 # 独立硬编码版本
-__version__ = "0.1.0"
+__version__ = "0.1.5.2"
 __author__ = "IntelliStream Team"
 __email__ = "shuhao_zhang@hust.edu.cn"
 __version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
