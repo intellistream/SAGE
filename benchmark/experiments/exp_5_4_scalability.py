c7fce793fde11fcfe3700e996241a6d9c3f115ad
Shuhao Zhang <shuhao_zhang@hust.edu.cn>
2026-01-06 14:13:16 +0800

    update

---
+++ benchmark/experiments/exp_5_4_scalability.py
@@ -0,0 +1,13 @@
+from sage.benchmark.benchmark_sage.experiments.control_plane_exp import (
+    ControlPlaneExperiment,
+)
+
+
+class ScalabilityExperiment(ControlPlaneExperiment):
+    """
+    Experiment for Scalability.
+
+    Focuses on throughput and latency under high load.
+    """
+
+    pass
