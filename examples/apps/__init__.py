1644673c1cae820e716b72e0d6589802a2847535
zslchase <1833842212@qq.com>
2026-01-08 02:22:05 +0000

    chore: remove migrated directories (examples and sage-apps)
    - examples/ has been migrated to sage-examples repository
    - packages/sage-apps/ has been migrated to sage-apps repository
    - These directories are now maintained as independent repositories

--- examples/apps/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-SAGE Applications Examples
-
-This package contains example scripts for running applications from sage-apps.
-"""
-
-__version__ = "0.1.0"
--- packages/sage-apps/tests/work_report_generator/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for Work Report Generator module"""
